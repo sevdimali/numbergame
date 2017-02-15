3121eb63c94bd4d2db4a83b3402f982d38efd5a9
sevdimali <sevdimaliisayev@mail.ru>
2017-02-16 00:09:47 +0400

    Add files via upload

---
+++ shell_number_game.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+chck=0
+rndnum=$((1 + RANDOM % 100))
+echo "--------Welcome to Number Game--------"
+echo "-                                    -"
+echo "- 1 ile 100 arasinda eded daxil edin -"
+echo "-                                    -"
+echo "--------------------------------------"
+while [ $chck -ne 1  ]
+do
+read usernum
+        if [ $usernum -eq $rndnum ]; then
+        echo "Super!!!  TAPDIN!!!"
+        chck=1
+        fi
+
+        if [ $usernum -lt $rndnum ]; then
+        echo "Kicikdir"
+        fi
+
+        if [ $usernum -gt $rndnum ]; then
+        echo "Boyukdur"
+        fi
+done
